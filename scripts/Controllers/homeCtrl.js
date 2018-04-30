2784d719aa09f620ea47c54fc16238b123f51312
Ashish Sharma <ashish136sharma@gmail.com>
2018-04-30 21:31:26 +0530

    first commit.setup application routes using ui-router

---
+++ scripts/Controllers/homeCtrl.js
@@ -0,0 +1,5 @@
+var myApp=angular.module("myApp");
+myApp.controller("homeCtrl",['$scope',function($scope){
+
+    $scope.abc="ashish";
+}])
---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+var app = angular.module("myApp", ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'HTML/home.html'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('welcomeUser', {
+            url: '/welcomeUser',
+            templateUrl: 'HTML/welcomeUser.html'       
+        })
+        .state('welcomeUser.aadhar', {
+            url: '/aadhar',
+            templateUrl: 'HTML/aadhar.html'       
+        });
+        $urlRouterProvider.otherwise('/home');
+$locationProvider.html5Mode(true);
+
+       
+});
+// app.config(function($routeProvider) {
+//     $routeProvider
+//     .when("/", {
+//         templateUrl : "\\HTML\\home.html",
+//         controller : "homeCtrl"
+//     })
+//     .when("/welcomeUser", {
+//         templateUrl : "\\HTML\\welcomeUser.html"
+//     })
+//     .when("/aadhar",{
+//         templateUrl : "\\HTML\\aadhar.html",
+//         controller : "homeCtrl"
+//     })
+// });
